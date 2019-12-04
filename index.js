468e10fd0fc1bc956af4255bbd001216b6c9eabd
IWANABETHATGUY <974153916@qq.com>
2019-12-04 23:59:03 +0800

    feat: finish

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 #!/usr/bin/env node
+const unusedToken = require('vscf-deadcode-detect').default;
 const os = require('os');
 const path = require('path');
-const program = require('commander')
+const fs = require('fs');
+const program = require('commander');
 program.version('0.1.0');
 program
- .option('-d, --directory [...otherdirs]', 'specify directory or file you want to scan')
+  .option('-c, --cpu <type>', 'specify directory or file you want to scan')
+  .command('<dir> [otherDirs...]');
 
 program.parse(process.argv);
 
-
-
-function commaSeparatedList(value, dummyPrevious) {
-  return value.split(',');
+const paths = program.args.map(p => {
+  return path.resolve(process.cwd(), p);
+});
+function walkPath(base, dirs) {
+  return dirs.reduce((pre, p) => {
+    const absolutePath = path.resolve(base, p);
+    if (fs.statSync(absolutePath).isDirectory()) {
+      return pre.concat(walkPath(absolutePath, fs.readdirSync(absolutePath)));
+    } else {
+      if (path.extname(p) === '.vue') {
+        pre.push(path.resolve(base, p));
+      }
+    }
+    return pre;
+  }, []);
 }
-console.log(program.directory)
+const vueFileList = paths.reduce((pre, p) => {
+  if (fs.statSync(p).isDirectory()) {
+    return pre.concat(walkPath(p, fs.readdirSync(p)));
+  } else {
+    if (path.extname(p) === '.vue') {
+      pre.push(p);
+    }
+  }
+  return pre;
+}, []);
+
+vueFileList.forEach(p => {
+  const file = fs.readFileSync(p).toString();
+  const unusedTokens = unusedToken(file);
+  if (unusedTokens.length) {
+    console.log(p);
+    console.log(unusedTokens);
+  }
+});
